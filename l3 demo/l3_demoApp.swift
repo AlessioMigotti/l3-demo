f130a3ef5c5bec29a7cf5e24cbf650d4db2ef765
Alessio <migottialessiodev@gmail.com>
2023-04-12 15:29:38 +0100

    Initial Commit

---
+++ l3 demo/l3_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  l3_demoApp.swift
+//  l3 demo
+//
+//  Created by alessio rodolfo migotti on 12/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct l3_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
